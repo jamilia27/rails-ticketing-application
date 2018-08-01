fa884b77b183d7f71e50700c5309895acecd28ac
Katie Weber <k.weber0893@gmail.com>
2018-08-01 16:47:32 -0400

    Added strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create; end
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_path(user)
+    else
+      redirect_to new_user_path
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :account_balance,
+      :password
+    )
+  end
 end
