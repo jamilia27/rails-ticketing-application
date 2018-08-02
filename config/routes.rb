e1cdcb18b00e4348de4bdb06f0800a99e423771f
Katie Weber <k.weber0893@gmail.com>
2018-08-02 09:02:10 -0400

    Added custom routing for signup and signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  resources :users
+  get "/signup", to: "users#new", as: "new_user"
+  get "/signin", to: "sessions#new", as: "new_session"
+  resources :users, only: [:index, :create, :show]
+  resources :sessions, only: [:create, :destroy]
   resources :concerts
   resources :tickets
 end
