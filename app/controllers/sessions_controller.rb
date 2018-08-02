85431f8740da5f3df9e3db89096d91a9560b9903
Katie Weber <k.weber0893@gmail.com>
2018-08-02 09:41:55 -0400

    Created signin form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,15 @@
 
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    user = User.find_by(email: params[:signin][:email].downcase)
+    if user && user.authenticate(params[:signin][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user), notice: "Successfully logged in!"
+    else
+      flash.now.alert = "Incorrect email or password, please try again."
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/signup", to: "users#new", as: "new_user"
   get "/signin", to: "sessions#new", as: "new_session"
+  post "/signin", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users, only: [:index, :create, :show]
-  resources :sessions, only: [:create, :destroy]
   resources :concerts
   resources :tickets
 end
