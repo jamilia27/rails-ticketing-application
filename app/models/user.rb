54a3e0cffb73158c6dc2f5ae200b82586978112c
Katie Weber <k.weber0893@gmail.com>
2018-08-02 09:20:41 -0400

    More validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :password, presence: true
   has_many :tickets
   has_many :concerts, through: :tickets
 end
