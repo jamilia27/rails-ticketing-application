46d59fd0f5694c88ce7da6325d3fed1810ec95de
Katie Weber <k.weber0893@gmail.com>
2018-08-02 10:57:47 -0400

    Wrote ticket purchasing method

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,33 @@
 class Ticket < ApplicationRecord
   belongs_to :concert
   belongs_to :user
+
+  def buy_ticket
+    if not_enough_money?
+      purchase_denied
+    else
+      buy_the_ticket
+      thank_you
+    end
+  end
+
+private
+
+  def not_enough_money?
+    true if user.account_balance < price
+  end
+
+  def purchase_denied
+    "Sorry your account balance is too low to purchase this ticket"
+  end
+
+  def buy_the_ticket
+    user.account_balance -= price
+    user.save
+  end
+
+  def thank_you
+    "Thanks for purchasing your ticket to #{concert.name}!"
+    "See you at #{concert.venue} on #{concer.date}!"
+  end
 end
