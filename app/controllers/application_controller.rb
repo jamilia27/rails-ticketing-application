a2fd32d324d09baf2119fa3a755a944d7b4acd10
Katie Weber <k.weber0893@gmail.com>
2018-08-02 10:13:29 -0400

    Corrected error and renamed routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def authorize
+    return unless current_user.nil?
+    redirect_to signin_path, alert: "You must be logged in to view this page."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "/signup", to: "users#new", as: "new_user"
-  get "/signin", to: "sessions#new", as: "new_session"
+  get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users, only: [:index, :create, :show]
