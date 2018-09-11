5724a36b5e34a85c255109df3ac3c29d78473af0
Katie Weber <k.weber0893@gmail.com>
2018-09-11 09:33:54 -0400

    Added strong params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,12 @@
 class ReviewsController < ApplicationController
 
   def create
+
+  end
+
+private
+
+  def review_params
+    params.require(:review).permit(:title, :body, :user_id, :concert_id)
   end
 end
