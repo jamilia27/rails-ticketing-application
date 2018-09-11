7abf332b3d9b9a3b07a170b14d63a0a5eec45d64
Katie Weber <k.weber0893@gmail.com>
2018-09-11 08:48:26 -0400

    Added review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :concert
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :title, length: { in: 1..30 }
+  validates :body, length: { in: 1..200 }
 end
