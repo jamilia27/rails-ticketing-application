af89a6e699d3cff7ba769b475476f1b97568e6f5
Katie Weber <k.weber0893@gmail.com>
2018-09-11 11:27:00 -0400

    Added form styling

--- app/assets/javascripts/review-form.js
+++ app/assets/javascripts/review-form.js
@@ -1,13 +1,17 @@
-$(function() {
-  $('#new_review').on("submit", function(e) {
-    e.preventDefault()
-    const values = $(this).serialize()
-    let posting = $.post('/reviews', values)
-    // debugger
-    posting.done(function(review) {
-      $(".reviewTitle").text(review["title"])
-      $(".reviewBody").text(review["body"])
-      $(".reviewedBy").text("Reviewed by: " + review["user"]["name"])
+// class newReview {
+//   appendReview() {
+    $(function() {
+      $('#new_review').on("submit", function(e) {
+        e.preventDefault()
+        const values = $(this).serialize()
+        let posting = $.post('/reviews', values)
+        // debugger
+        posting.done(function(review) {
+          $(".reviewTitle").text(review["title"])
+          $(".reviewBody").text(review["body"])
+          $(".reviewedBy").text("Reviewed by: " + review["user"]["name"])
+        })
+      })
     })
-  })
-})
+//   }
+// }
