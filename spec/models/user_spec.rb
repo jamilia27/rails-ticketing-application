2880edae14ef54ae9f6d1d1bbbb4640874c6faff
Katie Weber <k.weber0893@gmail.com>
2018-09-12 15:03:14 -0400

    Added shoulda matcher gem to help test validations

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+require 'test_helper'
+
+Rspec.describe User, type: :model do
+
+  describe "email" do
+    context "when signing up" do
+      it "is invalid without a unique email" do
+
+    end
+  end
+end
