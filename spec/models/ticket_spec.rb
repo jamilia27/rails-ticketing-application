4767fe3a7aa4b7b1371b2bec51d84b0643b865a3
Katie Weber <k.weber0893@gmail.com>
2018-09-12 16:35:55 -0400

    Finished validation testing and staged test for charge user method

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe Ticket, type: :model do
   it "has validations", :aggregate_failures do
     expect(subject).to validate_presence_of(:price)
     expect(subject).to validate_presence_of(:concert)
+    expect(subject).to validate_presence_of(:user)
+  end
+
+  describe ".charge_user" do
+    it "subtracts the tickets price from the user's account balance and saves the user" do
+    end
   end
 end
