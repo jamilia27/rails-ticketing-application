da01fa9f66bf3a50e85b52b42784d81a81fa72e5
Katie Weber <k.weber0893@gmail.com>
2018-09-12 10:54:31 -0400

    Utilizing turbolinks for nav

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -9,6 +9,9 @@ class ConcertsController < ApplicationController
     @ticket = Ticket.new
     @review = Review.new
     @concert = Concert.find_by(id: params[:id])
+    @previous_concert = Concert.find_by("id < :concert_id", concert_id: @concert.id) || Concert.last
+    @next_concert = Concert.find_by("id > :concert_id", concert_id: @concert.id) || Concert.first
+
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @concert.as_json.merge({review_url: reviews_path(concert_id: @concert.id)}) }
