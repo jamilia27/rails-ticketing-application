a6b95e3c52d8d3a8f1e6e233e3e047a3cded9492
Katie Weber <k.weber0893@gmail.com>
2018-09-12 16:34:01 -0400

    Added aggregate failures

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  it "belongs to a user and a concert" do
+  it "belongs to a user and a concert", :aggregate_failures do
     expect(subject).to belong_to(:user)
     expect(subject).to belong_to(:concert)
+  end
+
+  it "has validations", :aggregate_failures do
+    expect(subject).to validate_presence_of(:price)
+    expect(subject).to validate_presence_of(:concert)
   end
 end
