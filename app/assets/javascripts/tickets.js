84689c3db84a92a741ed0b93431bac2688d87b90
Katie Weber <k.weber0893@gmail.com>
2018-09-13 09:35:50 -0400

    Removed unnecessary comma from JS file

--- app/assets/javascripts/tickets.js
+++ app/assets/javascripts/tickets.js
@@ -1,8 +1,5 @@
 $(document).on("turbolinks:load", function() {
   $('#load-tickets').on('click', function() {
-    // e.preventDefault()
-    // // alert("This fucking sucks")
-    // let counter = 0
     const userId = $(this).data("id")
     const url = '/users/' + userId + '/tickets' + ".json"
     $.get(url, function(tickets) {
