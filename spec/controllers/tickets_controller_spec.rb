ba41644c95164d5c9c9d6748289a6c05fe28c957
Katie Weber <k.weber0893@gmail.com>
2018-09-13 08:37:17 -0400

    Wrote first controller test for tickets

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
-
+  describe "GET #index" do
+    it "shows all a users ticket's if logged in" do
+      user = create(:user)
+      session[:user_id] = user.id
+      get :index, params: {user_id: user.id}
+      expect(response.status).to eq(200)
+    end
+  end
 end
